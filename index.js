65c444301916c10b92ae2a61a94a216dc0e4f8a0
Dan <daramirezp1112@gmail.com>
2023-08-11 18:02:56 -0500

    Lab1

---
+++ index.js
@@ -0,0 +1,14 @@
+//dependency
+
+const calculator = require('birthday-calculator');
+
+const date = new Date ('2004,12,11');
+console.log(calculator.getDayWhenBorn(date))
+
+//devDependency
+
+const math = require('mathjs')
+
+console.log(math.sqrt(4))
+
+
